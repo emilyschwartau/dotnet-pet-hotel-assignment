5d164ae700928f6e5ffbdebd74184e8b7462e311
Eric Meinzer <emeinzer7@gmail.com>
2021-12-02 13:45:54 -0600

    set initial Pet.Models

--- Models/Pet.cs
+++ Models/Pet.cs
@@ -3,9 +3,53 @@ using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 using System;
 
-namespace pet_hotel
+
+
+namespace pet_hotel.Models
 {
-    public enum PetBreedType {}
-    public enum PetColorType {}
-    public class Pet {}
+    public enum PetBreedType
+    {
+        Shepard,
+        Poodle,
+        Beagle,
+        Bulldog,
+        Terrier,
+        Boxer,
+        Labrador,
+        Retriever,
+    }
+
+    public enum PetColorType
+    {
+        White,
+        Black,
+        Golden,
+        Tricolor,
+        Spotted,
+    }
+
+
+    public class Pet
+    {
+        public int id { get; set; }
+
+        [Required]
+        public string name { get; set; }
+
+        [Required]
+        [JsonConverter(typeof(JsonStringEnumConverter))]
+        public PetBreedType breedType { get; set; }
+
+        [Required]
+        [JsonConverter(typeof(JsonStringEnumConverter))]
+        public PetColorType colorType { get; set; }
+
+        [Required] // do we need Required? or simply use a ? after DateTime
+        public DateTime checkedInAt { get; set; }
+
+        [ForeignKey("PetOwners")]
+        public int petOwnerId { get; set; }
+        
+    }
+    
 }
