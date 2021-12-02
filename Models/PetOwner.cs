7ffe4e101bee021d084a2f9bfd8f6d25af501daa
Emily Schwartau <emilyschwartau+1@gmail.com>
2021-12-02 13:53:49 -0600

    owner model

--- Models/PetOwner.cs
+++ Models/PetOwner.cs
@@ -5,5 +5,28 @@ using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 namespace pet_hotel
 {
-    public class PetOwner {}
+    public class PetOwner 
+    {
+        public int id {get; set;}
+
+        [Required]
+        public string name {get; set;}
+
+        [Required]
+        //validated format?
+        public string email {get; set;}
+
+        [JsonIgnore]
+        public List<Pets> petList {get; set;}
+
+        [NotMapped]
+        public int petCount 
+        {
+            get {
+                return (this.petList != null ? this.petList.Count : 0);
+            }
+        }
+
+        
+    }
 }
