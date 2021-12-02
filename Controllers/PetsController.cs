48dedbbc60e9e660bcfcc4476b939158aea1097e
Chaoching Vang <chaoching.vang@gmail.com>
2021-12-02 14:59:08 -0600

    added get, getbyid, post petowner routes

--- Controllers/PetsController.cs
+++ Controllers/PetsController.cs
@@ -21,11 +21,11 @@ namespace pet_hotel.Controllers
 
         // This is just a stub for GET / to prevent any weird frontend errors that 
         // occur when the route is missing in this controller
-        // [HttpGet]
-        // public IEnumerable<Pet> GetPets()
-        // {
-        //     return new List<Pet>();
-        // }
+        [HttpGet]
+        public IEnumerable<Pet> GetPets()
+        {
+            return new List<Pet>();
+        }
 
         [HttpGet]
         [Route("test")]
